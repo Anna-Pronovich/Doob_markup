3e87f4b1cbea6160ecc829e2f01f12c2265575fc
Anna-Pronovich <pronovich.ann@gmail.com>
2019-09-06 16:17:42 +0300

    feat: add styles for main and footer

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ $('.menu').on('click', function (){
   $(this).toggleClass('active');
   $('.nav-overlay').toggleClass('menu-open');
 });
+
+$('.nav a').on('click', function (){
+  $('.menu').removeClass('active');
+  $('.nav-overlay').removeClass('menu-open');
+})
